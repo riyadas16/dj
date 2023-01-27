8e748a19bc7079b64a3dd1771affd03400521109
riya <riyudas2017@gmail>
2023-01-27 17:41:28 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '@/styles/globals.css'
 import Footer from './component/Footer'
 import Header from './component/Header'
-
+import Showcase from './component/Showcase'
+import { useRouter } from 'next/router'
 export default function App({ Component, pageProps }) {
-  return <><Header/><Component {...pageProps} />
+  const router = useRouter()
+  return <><Header/> {router.pathname === '/' && <Showcase/>} <Component {...pageProps} />
 <Footer/></>}
---
+++ pages/component/Showcase.js
@@ -0,0 +1,12 @@
+import styles from '../../styles/Showcase.module.css'
+
+function Showcase() {
+  return (
+    <div className={styles.showcase}>
+        <h1>Welcome</h1>
+        <h2>Find tht hottest dJ EVENTS</h2>
+ </div>
+  )
+}
+
+export default Showcase
